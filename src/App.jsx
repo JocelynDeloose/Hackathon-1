00a45787daf272033d52dd733f6e8af0ff0f1b6c
anthony <anthony.senechal59128@gmail.com>
2023-05-11 20:52:00 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import RulesPage from "./Page/RulesPage/RulesPage";
+import AboutUsPage from "./Page/AboutUsPage/AboutUsPage";
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Footer />}>
+            <Route path="/Rules" element={<RulesPage />} />
+            <Route path="/About-us" element={<AboutUsPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default App;
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,3 +1,23 @@
-function Footer() {}
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./FooterStyles.module.css";
+
+function Footer() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.wild}>© Wild Code School 2023</div>
+      <div>
+        <ul className={styles.list}>
+          <li className={styles.nav}>
+            <NavLink to="/Rules">Rules</NavLink>
+          </li>
+          <li className={styles.nav}>
+            <NavLink to="/About-us">About-us</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default Footer;
