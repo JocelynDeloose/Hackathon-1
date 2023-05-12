ed0c313e4f6032342bfe5085304f3a59ed6fbe73
SachaWildCode <sacha.loumachi@gmail.com>
2023-05-12 13:20:56 +0200

    rules fix

--- src/Page/RulesPage/RulesPage.jsx
+++ src/Page/RulesPage/RulesPage.jsx
@@ -2,54 +2,59 @@ import styles from "./RulesPageStyles.module.css";
 
 function RulesPage() {
   return (
-    <div className={styles.text}>
-      <h2>Castle Internal Regulations</h2>
+    <div className={styles.rules_container}>
+      <div className={styles.text}>
+        <h2>Castle Internal Regulations</h2>
 
-      <p>
-        The estate of the Castle, including its park, outbuildings and castle,
-        is private property. Therefore, it is governed by specific internal
-        regulations, and anyone wishing to visit or stay on the estate must
-        comply with the regulations below, under penalty of being denied access.
-      </p>
+        <p>
+          The estate of the Castle, including its park, outbuildings and castle,
+          is private property. Therefore, it is governed by specific internal
+          regulations, and anyone wishing to visit or stay on the estate must
+          comply with the regulations below, under penalty of being denied
+          access.
+        </p>
 
-      <p>
-        As you may know, our residence is connected to a mysterious shadow area
-        that could be considered paranormal. Therefore, access to the castle is
-        strongly discouraged for pregnant women and people with heart
-        conditions, and minors unaccompanied are prohibited.{" "}
-      </p>
+        <p>
+          As you may know, our residence is connected to a mysterious shadow
+          area that could be considered paranormal. Therefore, access to the
+          castle is strongly discouraged for pregnant women and people with
+          heart conditions, and minors unaccompanied are prohibited.{" "}
+        </p>
 
-      <p>
-        It is mandatory for anyone visiting the estate to spend one or more
-        nights to be in possession of the civil liability waiver that is
-        available on the site. Without it, access to the castle will be denied.
-      </p>
+        <p>
+          It is mandatory for anyone visiting the estate to spend one or more
+          nights to be in possession of the civil liability waiver that is
+          available on the site. Without it, access to the castle will be
+          denied.
+        </p>
 
-      <p>
-        The castle is a private property registered as a historical monument,
-        and visitors are required to respect the place and its history. Any
-        person engaging in a frivolous or suspicious attitude may be expelled
-        from the premises at the discretion of the owners. Any photo or video
-        taken out of context, aimed at tarnishing the image of the place and
-        posted online including the name of the estate may be subject to legal
-        action.
-      </p>
+        <p>
+          The castle is a private property registered as a historical monument,
+          and visitors are required to respect the place and its history. Any
+          person engaging in a frivolous or suspicious attitude may be expelled
+          from the premises at the discretion of the owners. Any photo or video
+          taken out of context, aimed at tarnishing the image of the place and
+          posted online including the name of the estate may be subject to legal
+          action.
+        </p>
 
-      <p>
-        If you plan to make a paranormal investigation film, please send us your
-        synopsis and request for filming authorization. Our animal friends are
-        not allowed on the property. Only the dining room fireplace may be used.
-        It is your duty to respect the boundaries indicated by the tape and not
-        to cross them under any circumstances. They indicate a dangerous
-        construction area strictly prohibited to the public.
-      </p>
+        <p>
+          If you plan to make a paranormal investigation film, please send us
+          your synopsis and request for filming authorization. Our animal
+          friends are not allowed on the property. Only the dining room
+          fireplace may be used. It is your duty to respect the boundaries
+          indicated by the tape and not to cross them under any circumstances.
+          They indicate a dangerous construction area strictly prohibited to the
+          public.
+        </p>
 
-      <p>
-        Alcohol must be consumed in moderation at the castle to avoid any
-        overflow and damage. Please download this civil liability waiver if you
-        wish to come to Bloody Castle, sign it, and also read and sign the
-        internal regulations. Download the civil liability waiver.
-      </p>
+        <p>
+          Alcohol must be consumed in moderation at the castle to avoid any
+          overflow and damage. Please download this civil liability waiver if
+          you wish to come to Bloody Castle, sign it, and also read and sign the
+          internal regulations. Download the civil liability waiver.
+        </p>
+      </div>
     </div>
   );
 }
