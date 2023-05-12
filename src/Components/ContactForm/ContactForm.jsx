716f8f3099d04ebb8ca3d842a9d89aef4cfba30b
SachaWildCode <sacha.loumachi@gmail.com>
2023-05-12 12:26:52 +0200

    form done

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import styles from "./ContactForm.module.css";
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -20,87 +20,89 @@ const ContactForm = () => {
     // Handle form submission here
   };
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <div className={styles.formLeft}>
-        <div className={styles.formGroup}>
-          <label htmlFor="nom">Nom</label>
-          <input
-            type="text"
-            id="nom"
-            name="nom"
-            value={formData.nom}
-            onChange={handleChange}
-            required
-          />
+    <div className={styles.contactFormContainer}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.formLeft}>
+          <div className={styles.formGroup}>
+            <label htmlFor="nom">Nom</label>
+            <input
+              type="text"
+              id="nom"
+              name="nom"
+              value={formData.nom}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
         </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
+        <div className={styles.formRight}>
+          <div className={styles.formGroup}>
+            <label htmlFor="prenom">Prénom</label>
+            <input
+              type="text"
+              id="prenom"
+              name="prenom"
+              value={formData.prenom}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="telephone">Téléphone</label>
+            <input
+              type="tel"
+              id="telephone"
+              name="telephone"
+              value={formData.telephone}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="groupeSanguin">Groupe sanguin</label>
+            <select
+              id="groupeSanguin"
+              name="groupeSanguin"
+              value={formData.groupeSanguin}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Choisissez votre groupe sanguin</option>
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="AB+">AB+</option>
+              <option value="AB-">AB-</option>
+              <option value="O+">O+</option>
+              <option value="O-">O-</option>
+            </select>
+          </div>
+          <button type="submit">Envoyer</button>
         </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="message">Message</label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          />
-        </div>
-      </div>
-      <div className={styles.formRight}>
-        <div className={styles.formGroup}>
-          <label htmlFor="prenom">Prénom</label>
-          <input
-            type="text"
-            id="prenom"
-            name="prenom"
-            value={formData.prenom}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="telephone">Téléphone</label>
-          <input
-            type="tel"
-            id="telephone"
-            name="telephone"
-            value={formData.telephone}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="groupeSanguin">Groupe sanguin</label>
-          <select
-            id="groupeSanguin"
-            name="groupeSanguin"
-            value={formData.groupeSanguin}
-            onChange={handleChange}
-            required
-          >
-            <option value="">Choisissez votre groupe sanguin</option>
-            <option value="A+">A+</option>
-            <option value="A-">A-</option>
-            <option value="B+">B+</option>
-            <option value="B-">B-</option>
-            <option value="AB+">AB+</option>
-            <option value="AB-">AB-</option>
-            <option value="O+">O+</option>
-            <option value="O-">O-</option>
-          </select>
-        </div>
-        <button type="submit">Envoyer</button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 export default ContactForm;
